4d84f5f47cba2574afe0dfe981b1d578a7fb4c9a
KovaKreative <eric.kovalevskyy@gmail.com>
2023-06-07 18:32:14 -0400

    Style game cards

--- src/components/GameCard.js
+++ src/components/GameCard.js
@@ -6,13 +6,13 @@ export default function GameCard(props) {
 
   return (
     <div className='GameCard'>
-      <img src={image} alt='Cover Image' />
+      <img className='cover' src={image} alt='Cover Image' />
       <div className='game-info'>
         <header>
           <h1>{title}</h1>
           <h4>Published: {new Date(published).toLocaleDateString('en-CA')}</h4>
         </header>
-        <p>{description}</p>
+        <p className='description'>{description}</p>
         <footer>
           <button href={url} target='_blank'>Visit</button>
         </footer>
