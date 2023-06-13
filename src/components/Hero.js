d5d60154acc8a2097c2833d6c1c09a64953c86a8
KovaKreative <eric.kovalevskyy@gmail.com>
2023-06-13 12:43:08 -0400

    Implement hero section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,9 +1,26 @@
 import './Hero.css';
 
+import banner from '../images/KovaGamesBanner.png';
+import bkg1 from '../images/bkg1.png';
+import bkg2 from '../images/bkg2.jpg';
+import bkg3 from '../images/bkg3.png';
+import bkg4 from '../images/bkg4.jpg';
+
 export default function Hero() {
+
+  const coverImages = [bkg1, bkg2, bkg3, bkg4];
+  const index = Math.floor(Math.random() * coverImages.length);
+  const coverImage = coverImages[index];
+
   return (
     <section className="Hero">
-
+      <img
+        className="hero-bg"
+        src={coverImage}
+        alt=""
+      />
+      <img className="logo" src={banner} />
+      <p className="tagline">KovaKreative is an indepentent solo video game designer and developer striving to make fun, visually engaging games that anyone can enjoy.</p>
     </section>
   );
 }
