cfc9064b5e46c7144a0bc0a3a7b7c80044ada224
KovaKreative <eric.kovalevskyy@gmail.com>
2023-06-13 12:43:19 -0400

    Add loading message

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,10 +1,11 @@
 import logo from '../images/LoadWheel.png';
 import './Loading.css';
 
-export default function Loading() {
+export default function Loading(props) {
   return (
     <div className="Loading">
       <img src={logo} className="wheel" />
+      <p>{props.message}</p>
     </div>
   );
 }
