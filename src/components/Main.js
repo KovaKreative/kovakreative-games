3443e1ecb4237ec4104057bdbce449da2cdb6c2a
KovaKreative <eric.kovalevskyy@gmail.com>
2023-06-16 12:08:11 -0400

    install unity webgl

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,30 @@
 import './Main.css';
 import GamesList from "./GamesList";
+import FeaturedGame from './FeaturedGame';
+
+import { useState } from 'react';
 
 export default function Main() {
-  return (<>
-    <GamesList />
-  </>);
+
+  const [screen, setScreen] = useState('games');
+
+  return (
+    <div className='Main'>
+      <div className='nav' onChange={(e) => {
+            setScreen(e.target.value);
+          }}>
+        <label>
+          <input type="radio" className="nes-radio is-dark" name="answer-dark" value="games" defaultChecked={true} />
+          <span>Games</span>
+        </label>
+        <label>
+          <input type="radio" className="nes-radio is-dark" name="answer-dark" value="invaders" />
+          <span>Kova Invaders</span>
+        </label>
+      </div>
+
+      {screen === 'games' && <GamesList />}
+      {screen === 'invaders' && <FeaturedGame />}
+    </div>
+  );
 }
