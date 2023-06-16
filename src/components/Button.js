23840f847f477218365a4ac3fa8bd3f1ccae87b8
KovaKreative <eric.kovalevskyy@gmail.com>
2023-06-16 11:03:07 -0400

    Adjust button style

--- src/components/Button.js
+++ src/components/Button.js
@@ -5,7 +5,7 @@ export default function Button(props) {
   return (
 
     <a
-      className="Button nes-btn is-primary"
+      className="Button nes-btn is-primary is-dark"
       href={props.url}
       target="_blank"
       rel="noreferrer"
